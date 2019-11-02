69bbec00abab5b0a0b03e6061cdf01786af2642d
gabriel Reis <greis9010@gmail.com>
2019-11-02 19:32:07 -0300

    Ajustando código e adicionando novo comando

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+
+//#region Intancias de APIs
+
 //instanciando a APIs 
 
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 
-//End instanciando APIs
-var appExp = express();
+//#endregion
+
+//#region Variaveis Globais
+
+const appExp = express();
 /*Definindo uma porta padrao para utilizar caso essa porta
 * estabelecida pelo servidor ou maquina onde se encontra em funcionamento
 */
@@ -16,11 +22,17 @@ const token = '899326021:AAEIY-qEA_ueqPUAHwNg1GBrbvv23m3TPI4';
 // Crie um bot que use 'polling' para buscar novas atualizações 
 const bot = new TelegramBot(token, {polling :  true });
 
+//#endregion
+
   //Traga todas as informações da msg enviada a mim, e print in console
 bot.on('message',(msg) => console.log('msg:', msg));
 
-/*Leia tudo que esta escrito depois do comand 'echo' e envia a mesma coisa 
-* para o nosso usuario */
+//#region Comand Echo
+
+/**
+ * Leia tudo que esta escrito depois do comand 'echo' e envia a mesma coisa 
+ * para o nosso usuario 
+*/
 bot.onText( /\/echo (.*)/, function( msg, match ){
   
   var fromId = msg.chat.id;
@@ -29,7 +41,11 @@ bot.onText( /\/echo (.*)/, function( msg, match ){
   
 });
 
-/*Envie a mensagem contida em "resp" quando for utilizado o comand '/start'*/ 
+//#endregion
+
+//#region Comand GRobot Iniciar
+
+//GRobot "Iniciar"
 bot.onText( /\/GRobot (Iniciar!)/, function(msg){
   
   var fromId = msg.chat.id;
@@ -37,13 +53,22 @@ bot.onText( /\/GRobot (Iniciar!)/, function(msg){
   bot.sendMessage(fromId, resp);
   
 });
+bot.onText( /\/GRobot (iniciar!)/, function(msg){
+  
+  var fromId = msg.chat.id;
+  var resp = "Opa, estou Ligado!";
+  bot.sendMessage(fromId, resp);
+  
+});
+//#endregion
+
 
 //adicionado saida HTML para a aplicação
 appExp.get('/', function(req, res) {
   res.send('<br><i>Server ON!</i>');
 });
 
-
+//Apos a conexao sucess! print mensagem OK
 appExp.listen(port, () => {
   console.log(`Serve ON in port: ${port}!`);
 });
